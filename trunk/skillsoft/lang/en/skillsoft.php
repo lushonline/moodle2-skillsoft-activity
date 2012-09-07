427cfdbd672934ad68a92f8d2c21c0c74c7b1ba1
martinholden1972@gmail.com <martinholden1972@gmail.com@547c969e-0326-a415-5041-3fd0c7a6b055>
2012-09-07 11:18:29 +0000

    Version: 2012090700 2.x build incorporating.
    Enhancement
    * New setting to support disabling of usage data feed from Skillport
    * New setting to support reset of Custom Report cycle
    
    Bug Fixes
    * Remove the ENUM entries from the db/install.xml
    * Fix invalid url for "Attempt n" nav bar link on report.
    
    For the Moodle 1.9.x module version. See http://code.google.com/p/moodle-skillsoft-activity/

--- trunk/skillsoft/lang/en/skillsoft.php
+++ trunk/skillsoft/lang/en/skillsoft.php
@@ -29,19 +29,19 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$string['pluginadministration'] = 'SkillSoft Asset';
-$string['pluginname'] = 'SkillSoft Asset';
+$string['pluginadministration'] = 'Skillsoft Asset';
+$string['pluginname'] = 'Skillsoft Asset';
 
-$string['modulename'] = 'SkillSoft Asset';
-$string['modulename_help'] = '<p>This module provides a way to simply create a new activity for a SkillSoft OLSA Hosted asset that will be tracked by Moodle</p>
+$string['modulename'] = 'Skillsoft Asset';
+$string['modulename_help'] = '<p>This module provides a way to simply create a new activity for a Skillsoft OLSA Hosted asset that will be tracked by Moodle</p>
 <p>All the information to launch the asset can be entered manually, or just the Asset ID can be entered and then the remaining information automatically retrieved from the OLSA Server.</p>
 <p>Any asset that provides a score generates GradeBook entries</p>
 
-<strong>SkillSoft Open Learning Services Architecture (OLSA)</strong>
-<p>The SkillSoft Open Learning Services Architecture (OLSA) is an enhancements to the SkillSoft Skillport LMS architecture.</p>
+<strong>Skillsoft Open Learning Services Architecture (OLSA)</strong>
+<p>The Skillsoft Open Learning Services Architecture (OLSA) is an enhancements to the Skillsoft Skillport LMS architecture.</p>
 <p>It is made up of two components:</p> 
 <ul>
-<li>LMS Interface - This provides a means for Moodle to launch SkillSoft assets using the industry standard AICC HACP method.</li>
+<li>LMS Interface - This provides a means for Moodle to launch Skillsoft assets using the industry standard AICC HACP method.</li>
 <li>Web Services - This provides a means for another system to interact with Skillport using standards based Web Services. These provide the functionality in this module to support retrieving of course metadata and synchronising progress between the systems</li>
 </ul>
 <p>OLSA Supports two tracking modes, which determines where the users progress data is stored</p>
@@ -49,14 +49,14 @@ $string['modulename_help'] = '<p>This module provides a way to simply create a n
 <li>Track to LMS - All usage data is returned immediately to Moodle</li>
 <li>Track to OLSA - All usage data is returned to OLSA Server, and synchronised back to Moodle via a cron task</li>
 </ul>
-<p>SkillSoft recommend the &quot;Track to OLSA&quot; mode as this enhances the functionality available to the user alllowing use of:</p>
+<p>Skillsoft recommend the &quot;Track to OLSA&quot; mode as this enhances the functionality available to the user alllowing use of:</p>
 <ul>
-<li>Download Capability - Using the SkillSoft Course Manager (SCM) a customer can allow users to download courses to use off line and all tracking and synchronisation of usage data is automatically handled by OLSA</li>
-<li>KnowledgeCenters - A SkillSoft KnowledgeCenter is a pre-packaged, user-friendly learning portal that allows learners instant access to trusted, targeted content. This content may include assets loaded for direct access in Moodle. By using &quot;Track to OLSA&quot; only a single training record of usage will exist.</li>
-<li>Search and Learn Connect - Using this interface a user can perform on-demand, integrated searches across the full library of SkillSoft resources they have access to and be directed to the specific book page, course topic or other tools they need for instant and relevant answers.</li>
+<li>Download Capability - Using the Skillsoft Course Manager (SCM) a customer can allow users to download courses to use off line and all tracking and synchronisation of usage data is automatically handled by OLSA</li>
+<li>KnowledgeCenters - A Skillsoft KnowledgeCenter is a pre-packaged, user-friendly learning portal that allows learners instant access to trusted, targeted content. This content may include assets loaded for direct access in Moodle. By using &quot;Track to OLSA&quot; only a single training record of usage will exist.</li>
+<li>Search and Learn Connect - Using this interface a user can perform on-demand, integrated searches across the full library of Skillsoft resources they have access to and be directed to the specific book page, course topic or other tools they need for instant and relevant answers.</li>
 </ul>';
-$string['modulenameplural'] = 'SkillSoft Assets';
-$string['noassets'] = 'No SkillSoft Assets';
+$string['modulenameplural'] = 'Skillsoft Assets';
+$string['noassets'] = 'No Skillsoft Assets';
 $string['skillsoft_na'] = 'N/A';
 
 $string['skillsoft_waitingalt'] = 'Please wait...';
@@ -65,22 +65,22 @@ $string['skillsoft_waitingalt'] = 'Please wait...';
 $string['skillsoft:viewreport'] = 'View Report for all users';
 
 //Settings.php
-$string['skillsoft_olsaendpoint'] = 'SkillSoft OLSA EndPoint';
-$string['skillsoft_olsaendpointdesc'] = 'The URI for the SkillSoft OLSA Web Services EndPoint, for example http://test.skillwsa.com/olsa/services/Olsa. The URI is case sensitive';
+$string['skillsoft_olsaendpoint'] = 'Skillsoft OLSA EndPoint';
+$string['skillsoft_olsaendpointdesc'] = 'The URI for the Skillsoft OLSA Web Services EndPoint, for example http://test.skillwsa.com/olsa/services/Olsa. The URI is case sensitive';
 
-$string['skillsoft_olsacustomerid'] = 'SkillSoft OLSA Customer ID';
+$string['skillsoft_olsacustomerid'] = 'Skillsoft OLSA Customer ID';
 $string['skillsoft_olsacustomeriddesc'] = 'The Customer ID used with OLSA for authentication';
 
-$string['skillsoft_olsasharedsecret'] = 'SkillSoft OLSA Shared Secret';
+$string['skillsoft_olsasharedsecret'] = 'Skillsoft OLSA Shared Secret';
 $string['skillsoft_olsasharedsecretdesc'] = 'The Shared Secret used with OLSA for authentication';
 
 $string['skillsoft_sessionpurge'] = 'Number of hours to keep sessionid';
 $string['skillsoft_sessionpurgedesc'] = 'The number of hours that sessionids are kept before purging during CRON run.';
 
-$string['skillsoft_trackingmode'] = 'SkillSoft Tracking Mode';
+$string['skillsoft_trackingmode'] = 'Skillsoft Tracking Mode';
 $string['skillsoft_trackingmodedesc'] = 'The mode the OLSA site is configured for, if Track to LMS results are returned to LMS using AICC. If Track to OLSA the results are stored in OLSA Server and need to be retrieved, options for this are On Demand Communications or via a custom report for previous 24-hrs data.';
 
-$string['skillsoft_useridentifier'] = 'Moodle/SkillSoft User Identifier';
+$string['skillsoft_useridentifier'] = 'Moodle/Skillsoft User Identifier';
 $string['skillsoft_useridentifierdesc'] = 'The user data field to use as common identifier between Moodle and OLSA. We recommend the Moodle user ID as this is a system generated value and will not change in Moodle even if the users Username is modified.';
 $string['skillsoft_userid_identifier'] = 'ID';
 $string['skillsoft_username_identifier'] = 'Username';
@@ -97,10 +97,10 @@ $string['skillsoft_sso_actiontypedesc'] = 'Select the actiontype for launching a
 $string['skillsoft_sso_actiontype_launch'] = 'Launch Asset without showing Skillport UI (launch)';
 $string['skillsoft_sso_actiontype_summary'] = 'Launch Asset Summary Page in Skillport UI (summary)';
 
-$string['skillsoft_defaultssogroup'] = 'SkillSoft Default Group List';
+$string['skillsoft_defaultssogroup'] = 'Skillsoft Default Group List';
 $string['skillsoft_defaultssogroupdesc'] = 'A comma seperated list of the default groups to send for new users during SSO to Skillport. Existing users group membership in Skillport is not altered.';
 
-$string['skillsoft_settingsmissing'] = 'Can not retrieve SkillSoft OLSA Settings: please check the configuration settings.';
+$string['skillsoft_settingsmissing'] = 'Can not retrieve Skillsoft OLSA Settings: please check the configuration settings.';
 
 $string['skillsoft_accountprefix'] = 'Account Prefix';
 $string['skillsoft_accountprefixdesc'] = 'Enter a prefix which will be added in front of the username sent to Skillport.';
@@ -114,22 +114,28 @@ $string['skillsoft_reportincludetodaydesc'] = 'The report defaults to including 
 $string['skillsoft_clearwsdlcache'] = 'Clear the cached WSDL files';
 $string['skillsoft_clearwsdlcachedesc'] = 'The WSDL files are downloaded and cached to improve SOAP client startup time, if selected this will force them to be downloaded again and recached.';
 
+$string['skillsoft_disableusagedatacrontask'] = 'Disable the usage data task';
+$string['skillsoft_disableusagedatacrontaskdesc'] = 'When using "Track to OLSA" this allows you to disable the normal task that runs during CRON to synchronise the usage data from Skillport';
+
+$string['skillsoft_resetcustomreportcrontask'] = 'Reset the custom report cycle';
+$string['skillsoft_resetcustomreportcrontaskdesc'] = 'When using "Track to OLSA (Custom Report)" there are a number of steps: RUN - Request the report be generated, POLL - Check the report is ready, DOWNLOAD - Retrieve the report, IMPORT - Import the report data into Moodle database, PROCESS - Add the usage data to the users and activities. This allows you to reset the cycle to the RUN step on next CRON run';
+
 $string['skillsoft_usesso'] = 'Use OLSA SSO';
 $string['skillsoft_usessodesc'] = 'Use the OLSA Web Services SSO function, this requires one of the Track to OLSA modes. If unchecked all launches uses the AICC launch process';
 
 
 //mod_form.php
 $string['skillsoft_assetid'] = 'Asset ID';
-$string['skillsoft_assetid_help'] = '<p>This is the SkillSoft unique id that is assigned to all eLearning assets</p>
+$string['skillsoft_assetid_help'] = '<p>This is the Skillsoft unique id that is assigned to all eLearning assets</p>
 
 <strong>Locating Catalog Asset IDs</strong>
 <p>Catalog Assets are defined as those that appear within the OLSA platforms catalog view.</p>
-<p>In the catalog view all SkillSoft e-Learning courses, KnowledgeCenters and Learning Programmes are listed.</p>
-<p>What is not listed are Books24x7 assets or supplementary material such as SkillSoft JobAids or SkillBriefs.</p>
-<p>To retrieve a full list of IDs for all the avilable assets on your OLSA site. Login to the OLSA site as an administrator, your SkillSoft Account Team will provide the user id and password to use, and run a Course Listing Report.</p>
+<p>In the catalog view all Skillsoft elearning courses, KnowledgeCenters and Learning Programmes are listed.</p>
+<p>What is not listed are Books24x7 assets or supplementary material such as Skillsoft JobAids or SkillBriefs.</p>
+<p>To retrieve a full list of IDs for all the avilable assets on your OLSA site. Login to the OLSA site as an administrator, your Skillsoft Account Team will provide the user id and password to use, and run a Course Listing Report.</p>
 <p>The value to enter as the &quot;Asset ID&quot; is the &quot;Course Number&quot;</p>
 <strong>Locating Book Asset IDs</strong>
-<p>Login to the Books24x7 site as an administrator, your SkillSoft Account Team will provide the user id and password to use, and run:</p> 
+<p>Login to the Books24x7 site as an administrator, your Skillsoft Account Team will provide the user id and password to use, and run:</p> 
 <ul>
 <li><p>Titles in Collection - This report provides a list of all titles available in the selected collection listed alphabetically. It also includes coming soon titles. Specific data included:</p>
 <p>Publisher/Producer, title, B24 bookid, ISBN, copyright, date added to site. Speaker and expire date are also include for video based collections</p>
--- trunk/skillsoft/version.php
+++ trunk/skillsoft/version.php
@@ -30,7 +30,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2012081000;  // If version == 0 then module will not be installed
+$module->version  = 2012090700;  // If version == 0 then module will not be installed
 $module->requires = 2010080300;  // Requires this Moodle version
 $module->cron     = 60;           // Period for cron to check this module (secs)
 
