775089e38de0ced34666e10fa65feb89570b95c1
martinholden1972@googlemail.com <martinholden1972@googlemail.com@547c969e-0326-a415-5041-3fd0c7a6b055>
2013-05-31 12:14:08 +0000

    Initial commit of a custom Moodle System report to display all the TDR records stored in Moodle

---
+++ skillsoft_tdrreport/lang/en/report_skillsoft_tdrreport.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for skillsoft_tdrreport report
+ *
+ * @package    report_skillsoft_tdrreport
+ * @copyright  2013 onwards Martin Holden}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Skillsoft TDR Report Audit Log';
+$string['skillsoft_report_menu'] = 'Skillsoft Reports';
+$string['skillsoft_tdrreport:view'] = 'View Skillsoft TDR Report Audit Log';
+
+//Column Headers
+$string['skillsoft_tdrreport_tdrid'] = 'TDR ID';
+$string['skillsoft_tdrreport_timestamp'] = 'TDR Timestamp';
+$string['skillsoft_tdrreport_userid'] = 'User';
+$string['skillsoft_tdrreport_username'] = 'OLSA Username';
+$string['skillsoft_tdrreport_assetid'] = 'Asset ID';
+$string['skillsoft_tdrreport_data'] = 'TDR Data';
+$string['skillsoft_tdrreport_processed'] = 'Processed';
+
+$string['skillsoft_tdrreport_filterlabel'] = 'Moodle User ID';
+$string['skillsoft_tdrreport_applyfilter'] = 'Filter';
+$string['skillsoft_tdrreport_recordcount'] = 'Records found: ';
+
+$string['skillsoft_tdrreport_filename'] = 'skillsoft_tdrreport_auditlog';
